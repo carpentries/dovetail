a427c16e34da4fc1ff9800b2d388c4091ebd199b
Zhian N. Kamvar <zkamvar@gmail.com>
2020-06-08 12:33:06 -0700

    add examples

--- R/blocks.R
+++ R/blocks.R
@@ -3,6 +3,10 @@
 #' @return somethng
 #' @export
 #' @importFrom roxygen2 roxy_tag_parse
+#' @example
+#' soln <- system.file('inst', 'extdata', 'example-solution-only.R',
+#'   package = 'roxyblox')
+#' roxygen2::parse_file(soln, env = NULL)
 roxy_tag_parse.roxy_tag_solution <- function(x) {
   tag_markdown(x)
 }
@@ -12,6 +16,10 @@ roxy_tag_parse.roxy_tag_solution <- function(x) {
 #' @return somethng
 #' @export
 #' @importFrom roxygen2 roxy_tag_parse
+#' @example
+#' soln <- system.file('inst', 'extdata', 'example-number-echo.R',
+#'   package = 'roxyblox')
+#' roxygen2::parse_file(soln, env = NULL)
 roxy_tag_parse.roxy_tag_challenge <- function(x) {
   tag_markdown(x)
 }
---
+++ inst/extdata/example-solution-only.R
@@ -0,0 +1,20 @@
+#' @challenge
+#' ## Challenge 1
+#'
+#' Write a function called `kelvin_to_celsius()` that takes a temperature in
+#' Kelvin and returns that temperature in Celsius.
+#'
+#' Hint: To convert from Kelvin to Celsius you subtract 273.15
+#'
+#' @solution
+#'
+#' ## Solution to challenge 1
+#'
+#' Write a function called `kelvin_to_celsius` that takes a temperature in Kelvin
+#' and returns that temperature in Celsius
+#+{r}
+kelvin_to_celsius <- function(temp) {
+  celsius <- temp - 273.15
+  return(celsius)
+}
+#'
