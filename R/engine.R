62d6322332828a3e880447ae23c0256c98b302d1
Zhian N. Kamvar <zkamvar@gmail.com>
2020-06-15 16:46:28 -0700

    update engine
    This now explicitly searches for the last knitr call and uses the envir argument from there so that everything is matched up.

--- R/engine.R
+++ R/engine.R
@@ -10,10 +10,27 @@
 ## Currently not in use
 engine_generic_carp <- function(class) {
   function(options) {
+    # Find the position of the knitr call in the call stack
+    cstack <- vapply(sys.calls(), function(i) paste(as.character(deparse(i)), collapse = "\n"), character(1))
+    # The Frame stack gives the the parent frames of each call
+    fstack <- sys.frames()
+    # When I find which one is from knitr, I can get the "envir" variable
+    # from that frame, which tells me what environment was used to knit the
+    # initial document.
+    knitting <- rev(grep("knit(", cstack, fixed = TRUE))[[1]]
+    e <- get("envir", fstack[[knitting]])
+
     res <- parse_block(paste(options$code, collapse = "\n"))
-    extract_chunks(res)
-    res
-    paste(res, collapse = "\n")
+    tmp <- tempfile(fileext = ".md")
+    on.exit(unlink(tmp))
+    knitr::knit(
+      output = tmp,
+      text = res,
+      encoding = "UTF-8",
+      envir = e #parent.frame(n = frame_number)
+    )
+    out <- readLines(tmp)
+    paste(out, collapse = "\n")
   }
 }
 
