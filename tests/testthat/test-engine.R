263a2a399194dbc56599705d5793a9f94acb5ad5
Zhian N. Kamvar <zkamvar@gmail.com>
2020-06-16 14:22:53 -0700

    Add more tests and fix markdown

--- tests/testthat/test-engine.R
+++ tests/testthat/test-engine.R
@@ -13,11 +13,46 @@ test_that("engines work with calls to knitr", {
   tmp <- tempfile(fileext = ".md")
   on.exit(file.remove(tmp))
 
+  n_words <- 9
+  n_char <- 42
   expect_output({
     knitr::knit(f, output = tmp, envir = new.env(), encoding = "UTF-8")
   }, 'engine: chr "challenge"', fixed = TRUE)
 
   txt <- paste(readLines(tmp), collapse = "\n")
   expect_match(txt, "Now that we know that there are 6 words and 19 characters", fixed = TRUE)
+
+  expect_output({
+    knitr::knit(f, output = tmp, encoding = "UTF-8")
+  }, 'engine: chr "challenge"', fixed = TRUE)
+
+  txt <- paste(readLines(tmp), collapse = "\n")
+  expect_match(txt, "Now that we know that there are 6 words and 19 characters", fixed = TRUE)
+
+
+})
+
+test_that("engines work with calls to rmarkdown", {
+
+  f <- system.file('extdata', 'test-engine.Rmd', package = "roxyblox")
+  tmp <- tempfile(fileext = ".md")
+  on.exit(file.remove(tmp))
+
+  n_words <- 9
+  n_char <- 42
+  expect_output({
+    rmarkdown::render(f, output_file = tmp, envir = new.env(), encoding = "UTF-8")
+  }, 'engine: chr "challenge"', fixed = TRUE)
+
+  txt <- paste(readLines(tmp), collapse = "\n")
+  expect_match(txt, "Now that we know that there are 6 words and 19 characters", fixed = TRUE)
+
+  expect_output({
+    rmarkdown::render(f, output_file = tmp, encoding = "UTF-8")
+  }, 'engine: chr "challenge"', fixed = TRUE)
+
+  txt <- paste(readLines(tmp), collapse = "\n")
+  expect_match(txt, "Now that we know that there are 6 words and 19 characters", fixed = TRUE)
+
 
 })
