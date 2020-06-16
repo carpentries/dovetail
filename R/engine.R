195e3b639aeadd6881dd58eed94c4a60420409dd
Zhian N. Kamvar <zkamvar@gmail.com>
2020-06-16 13:57:42 -0700

    update engine to use knitr's environment

--- R/engine.R
+++ R/engine.R
@@ -10,16 +10,6 @@
 ## Currently not in use
 engine_generic_carp <- function(class) {
   function(options) {
-    # Find the position of the knitr call in the call stack
-    cstack <- vapply(sys.calls(), function(i) paste(as.character(deparse(i)), collapse = "\n"), character(1))
-    # The Frame stack gives the the parent frames of each call
-    fstack <- sys.frames()
-    # When I find which one is from knitr, I can get the "envir" variable
-    # from that frame, which tells me what environment was used to knit the
-    # initial document.
-    knitting <- rev(grep("knit(", cstack, fixed = TRUE))[[1]]
-    e <- get("envir", fstack[[knitting]])
-
     res <- parse_block(paste(options$code, collapse = "\n"))
     tmp <- tempfile(fileext = ".md")
     on.exit(unlink(tmp))
@@ -27,7 +17,8 @@ engine_generic_carp <- function(class) {
       output = tmp,
       text = res,
       encoding = "UTF-8",
-      envir = e #parent.frame(n = frame_number)
+      # https://stackoverflow.com/a/62417329/2752888
+      envir = knitr::knit_global()
     )
     out <- readLines(tmp)
     paste(out, collapse = "\n")
