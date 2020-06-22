37bc97a15dc29fd26060e2238f5d0de5ed9e7073
Zhian N. Kamvar <zkamvar@gmail.com>
2020-06-22 15:10:51 -0700

    conversion script adds dovetail to setup chunk

--- inst/convert/convert.R
+++ inst/convert/convert.R
@@ -9,6 +9,7 @@ library("xml2")
 # Grab the current lesson
 # l <- get_lesson("swcarpentry/r-novice-inflammation", rmd = TRUE)
 l <- Lesson$new(".", rmd = TRUE)
+
 make_r_chunks <- function(cblocks) {
   # Regex to find the beginning of a code chunk that is possibly an R chunk
   r_chunk <- "```\n(?!(#'|$))"
@@ -19,6 +20,19 @@ make_r_chunks <- function(cblocks) {
   xml2::xml_text(blocks) <- new_text
 }
 
+use_dovetail <- function(body) {
+  setup <- xml2::xml_find_first(body, ".//d1:code_block[contains(text(), '../bin/chunk-options.R')]")
+  txt   <- xml2::xml_text(setup)
+  txt   <- gsub(
+    "source\\(.../bin/chunk-options.R.\\)", 
+    "library('dovetail')\nsource(dvt_opts())",
+    txt
+  )
+  xml2::xml_text(setup) <- txt
+}
+
+# Fix setup
+walk(l$episodes, ~.x$body %>% use_dovetail())
 # Transform the blocks to chunks
 walk(l$episodes, ~.x$unblock()$code %>% make_r_chunks())
 # Overwrite the files
